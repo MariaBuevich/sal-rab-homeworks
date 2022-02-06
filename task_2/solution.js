9ec639ed48f39114701b7c4c433d96e641781062
MariaBuevich <98704172+MariaBuevich@users.noreply.github.com>
2022-02-06 11:26:36 +0300

    update task 2

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,11 +6,9 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
     let shippingSum;
-    if (productsSum === 0) {
+    if (productsSum === 0 || productsSum >= freeShippingMinSum) {
     shippingSum = 0;
-    } else if (productsSum >= freeShippingMinSum) {
-    shippingSum = 0;
-    } else if (productsSum > 0 && productsSum < freeShippingMinSum) {
+    }  else {
     shippingSum = shippingPrice;
     }
     
@@ -37,14 +35,14 @@ function calcDiscount(sum, min, discount) {
 
     // Задание №2.2. Рассчитать скидку
 
-    let discountSum;
+    // let discountSum;
 
-    if (productsSum >= discountMinSum) {
-        discountSum = productsSum * discountPart / 100
-    } else if (productsSum < discountMinSum) {
-        discountSum = 0; 
-    } 
-    
+    // if (productsSum >= discountMinSum) {
+    //     discountSum = productsSum * discountPart / 100
+    // } else if (productsSum < discountMinSum) {
+    //     discountSum = 0; 
+    // } 
+    let discountSum = productsSum >= discountMinSum ? productsSum * discountPart / 100 : 0;
     // создайте переменную discountSum
 
     // если productsSum больше или равно discountMinSum,
@@ -79,7 +77,8 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // } else {
     //     freeShipping = false;
     // }
-    freeShipping = !shippingSum;
+    // freeShipping = !shippingSum;
+    freeShipping = shippingSum === 0;
 
     // прибавьте к totalSum значение shippingSum
 
