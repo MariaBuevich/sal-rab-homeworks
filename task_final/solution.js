da2880e8dd6bd16690aec9e907e810f5dde732d8
MariaBuevich <98704172+MariaBuevich@users.noreply.github.com>
2022-02-13 03:59:28 +0300

    final task done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,21 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {client: '', order: {}, goods: []};
     let countOfGoods = goods.length;
+    for (let i = 0; i < countOfGoods; i += 1) {
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+        let objectGood = {
+            title: goods[i].title,
+            count: goods[i].count
+        };
+        data.goods.push(objectGood);
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
+    let superAddress = 'ул. '+ address.street + ', дом '+ address.house + ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж, кв ' + address.flat;
+    data.order.address = superAddress;
+    data.order.sum =  sum;
+    data.client = name + ' ' + phone;
+    let doneData = JSON.stringify({data});
+    return doneData;
 }
